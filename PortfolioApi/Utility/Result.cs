2329468a45f94598d8b29ef80226bc8371dcbbec
cfrank3N <adamcecarfrank@gmail.com>
2025-09-25 22:24:46 +0200

    Added Result<T> class for Services to send Success or failure + data

---
+++ PortfolioApi/Utility/Result.cs
@@ -0,0 +1,15 @@
+﻿namespace PortfolioApi.Utility
+{
+    public class Result<T>
+    {
+        public bool Successful { get; set; }
+        public string Message { get; set; }
+        // Nullable in case of Failure
+        // Needs to be generic because we need to send lists and single objects etc.
+        public T? Data { get; set; }
+
+        public static Result<T> Success(T data, string message) => new Result<T> { Successful = true, Data = data, Message = message };
+        public static Result<T> Failure(string message) => new Result<T> { Successful = false, Message = message };
+
+    }
+}
