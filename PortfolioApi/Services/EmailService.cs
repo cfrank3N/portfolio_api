cc1d04bcaeb10ad5c2c8c4b59c71648a7f9e7688
cfrank3N <adamcecarfrank@gmail.com>
2025-09-26 00:54:35 +0200

    added packages for BrevoApi and RestSharp. dotnet add package brevo_csharp --version 1.1.1/RestSharp

---
+++ PortfolioApi/Services/EmailService.cs
@@ -0,0 +1,12 @@
+﻿using PortfolioApi.Interfaces;
+
+namespace PortfolioApi.Services
+{
+    public class EmailService : IEmailService
+    {
+        public bool SendEmail(MessageProcessingHandler message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
