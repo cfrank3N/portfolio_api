8d938be7ad584593a355559789073554a0c6daeb
cfrank3N <adamcecarfrank@gmail.com>
2025-09-26 00:45:54 +0200

    Added IEmailService

---
+++ PortfolioApi/Interfaces/IEmailService.cs
@@ -0,0 +1,10 @@
+﻿using PortfolioApi.Entities;
+using PortfolioApi.Utility;
+
+namespace PortfolioApi.Interfaces
+{
+    public interface IEmailService
+    {
+        bool SendEmail(MessageProcessingHandler message);
+    }
+}
