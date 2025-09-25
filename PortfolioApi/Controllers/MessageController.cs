5d2938656e752206cc39d9744972b64ec02244bd
cfrank3N <adamcecarfrank@gmail.com>
2025-09-25 22:53:47 +0200

    fully implemented the controller to call the MessageService and return ActionResult to generate correct Http response codes

--- PortfolioApi/Controllers/MessageController.cs
+++ PortfolioApi/Controllers/MessageController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using PortfolioApi.Entities;
 using PortfolioApi.Services;
 
 namespace PortfolioApi.Controllers
@@ -14,6 +15,18 @@ namespace PortfolioApi.Controllers
         public MessageController(MessageService service)
         {
             _service = service;
+        }
+
+        [HttpPost("savemessage")]
+        public async Task<ActionResult<Message>> SaveMessageToDatabase([FromBody] Message message)
+        {
+            var result = await _service.SaveMessage(message);
+
+            if (!result.Successful)
+            {
+                return BadRequest(result.Message);
+            }
+            return CreatedAtAction(null, null, message);
         }
 
     }
