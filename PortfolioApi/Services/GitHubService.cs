95029980d30407ab44c70efe895c7d6e402ac13f
cfrank3N <adamcecarfrank@gmail.com>
2025-09-29 16:04:46 +0200

    Added a query to Githubs graphql api

---
+++ PortfolioApi/Services/GitHubService.cs
@@ -0,0 +1,48 @@
+﻿using Octokit.GraphQL;
+using Octokit.GraphQL.Core;
+using Octokit.GraphQL.Model;
+using static Octokit.GraphQL.Variable;
+
+namespace PortfolioApi.Services
+{
+    public class GitHubService
+    {
+        private readonly IConfiguration _configuration;
+        private readonly Connection _connection;
+
+        public GitHubService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _connection = new Connection(new ProductHeaderValue("PortfolioApi"), 
+                _configuration["ApiKeys:GitHubToken"]);
+        }
+
+        public async Task GetPinnedRepos()
+        {
+            // Create a new query and instantiate
+            // a list of objects of type GitHubRepo
+            
+            var query = new Query()
+               .User("cfrank3N")
+               .PinnedItems(first:6, types: new[] { PinnableItemType.Repository } )
+               .Nodes
+               .OfType<Repository>()
+               .Select(repo => new
+               {
+                   repo.Name,
+                   repo.Description,
+                   repo.Url
+               })
+               .Compile();
+
+            var result = await _connection.Run(query);
+
+            foreach (var repo in result)
+            {
+                Console.WriteLine(repo.Url);
+                Console.WriteLine(repo.GetType);
+            }
+
+        }
+    }
+}
